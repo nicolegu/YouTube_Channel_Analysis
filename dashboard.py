0763a1b3ee1f01698c2c098f8383de5de3d6001c
Zhen Gu <nicole.gu727@gmail.com>
2025-10-12 23:38:36 -0700

    Created line chart and table on Streamlit dashboard

---
+++ dashboard.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+import sqlite3
+from datetime import datetime, timedelta
+
+st.set_page_config(page_title='Stationery Channel Analytics', layout='wide')
+
+st.title
